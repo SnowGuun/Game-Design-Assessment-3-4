c13079596b30554f65901b21952066105cee4e56
SnowGuun <phnganhnguyn0323@gmail.com>
2023-04-02 22:34:39 +1000

    Added cube movement to go into loop

--- Assets/Scripts/PoliceController.cs
+++ Assets/Scripts/PoliceController.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class PoliceController : MonoBehaviour
 {
-    public Transform Player;
-    public float distance = 5.0f;
-    public float speed = 2.0f;
+    public float speed = 5.0f;
+    public Vector3 pointA = new Vector3(10f, 5f, 9f);
+    public Vector3 pointB = new Vector3(32f, 5f, 9f);
+    public Vector3 pointC = new Vector3(32f, 5f, 32f);
+    public Vector3 pointD = new Vector3(10f, 5f, 32f);
+    
+    private Vector3 currentTarget;
+
+    void Start()
+    {        
+        currentTarget = pointB;
+    }
 
     void Update()
     {
-        Vector3 playerPos = Player.position;
-        playerPos.x += distance;
-        transform.position = Vector3.Lerp(transform.position, playerPos, speed * Time.deltaTime);
-        
+
+        if (Vector3.Distance(transform.position, currentTarget) < 0.1f){
+            if (currentTarget == pointA)
+            {
+                currentTarget = pointB;
+            }
+            else if (currentTarget == pointB)
+            {
+                currentTarget = pointC;
+            }
+            else if (currentTarget == pointC)
+            {
+                currentTarget = pointD;
+            }
+            else
+            {
+                currentTarget = pointA;
+
+            }
+
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+
+      
     }
+
 }
 
