a671c078ac7309752a2c713686176f5a93f30b25
Joel3218 <13642499@student.uts.edu.au>
2023-04-13 13:29:19 +1000

    completed item pick functionality

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInventory : MonoBehaviour
 {
     public int NumberOfBooks { get; private set; }
 
+    public UnityEvent<PlayerInventory> onBookCollected;
+
     public void BookCollected()
     {
         NumberOfBooks++;
+        onBookCollected.Invoke(this);
     }
 }
