3b9c1463d36128eddab5477f7271d2b00fcb78da
Joel3218 <13642499@student.uts.edu.au>
2023-04-22 15:14:28 +1000

    game over screen almost complete

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool isGameOver = false;
+
+    public Transform Player;
+
+    [SerializeField] GameObject gameOverMenu;
+
+    CursorLockMode desiredMode;
+
+
+    void update()
     {
+        if (isGameOver)
+        {
+            GameOver();
+        }
+        else
+        {
+            GameNotOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        isGameOver = false;
+        Cursor.visible = false;
+        desiredMode = CursorLockMode.Confined;
+    }
+
+    public void GameNotOver()
+    {
+        isGameOver = false;
+        Cursor.visible = true;
+        desiredMode = CursorLockMode.None;
+        {
+            Cursor.lockState = desiredMode;
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
+        Debug.Log("Quit");
     }
+
 }
