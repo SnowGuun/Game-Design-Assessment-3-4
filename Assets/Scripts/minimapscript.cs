b909aed9fc778fbfa54fb6dfb8080dac25ca9122
SnowGuun <phnganhnguyn0323@gmail.com>
2023-04-26 23:13:59 +1000

    update

--- Assets/Scripts/minimapscript.cs
+++ Assets/Scripts/minimapscript.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class minimapscript : MonoBehaviour
 {
     public Transform player;
+    public Transform playerCamera;
 
-    private void LateUpdate()
+    private void Start()
+    {
+        playerCamera = player.GetComponentInChildren<Camera>().transform;
+    }
+
+
+    void LateUpdate()
     {
         Vector3 newPosition = player.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90.0f, playerCamera.rotation.eulerAngles.y, 0.0f);
+
+
     }
 }
