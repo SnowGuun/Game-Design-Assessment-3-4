16972f748d02a0450e140c5dbf04a07a0b38e3a7
Resipate <105903976+Resipate@users.noreply.github.com>
2023-05-06 13:47:29 +1000

    reworked tutorial

---
+++ Assets/Scripts/PromptEnabler.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromptEnabler : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject prompt;
+    [SerializeField] private GameObject altPrompt;
+    [SerializeField] private GameObject promptBackDrop;
+    public bool promptActive = false;
+    
+    private void OnTriggerEnter(Collider other) {
+        if(other.transform.name == "Player"){
+            if(altPrompt == null){
+                prompt.SetActive(true);
+                promptBackDrop.SetActive(true);
+                Time.timeScale = 0.0f;
+                promptActive = true;
+            }
+            else{
+                PlayerInventory pI = player.gameObject.GetComponent<PlayerInventory>();
+                if(pI.NumberOfBooks >= 3){
+                    prompt.SetActive(true);
+                    promptBackDrop.SetActive(true);
+                    Time.timeScale = 0.0f;
+                    promptActive = true;
+                }
+                else{
+                    altPrompt.SetActive(true);
+                    promptBackDrop.SetActive(true);
+                    Time.timeScale = 0.0f;
+                    promptActive = true;
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(promptActive){
+            if(Input.anyKeyDown){
+                if(altPrompt != null){ altPrompt.SetActive(false); }
+                prompt.SetActive(false);
+                promptBackDrop.SetActive(false);
+                promptActive = false;
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+}
